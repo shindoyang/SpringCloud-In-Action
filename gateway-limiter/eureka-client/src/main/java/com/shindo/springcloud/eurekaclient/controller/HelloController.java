563a3837a5b6e72ccb163df99e19cb1111fb138b
unknown <yanggeng@ut.cn>
2020-07-08 15:07:17 +0800

    新增eureka-client 修复2.2生成的test类报错，参考：https://blog.csdn.net/kmesky/article/details/102984592

---
+++ gateway-limiter/eureka-client/src/main/java/com/shindo/springcloud/eurekaclient/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.shindo.springcloud.eurekaclient.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author:
+ * @Description:
+ * @Date: Create in 2020/7/8
+ * @Modified By:
+ * @Modified Date:
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping("/rateLimit")
+    public String hello() {
+        return "hello, spring cloud Gateway";
+    }
+}
