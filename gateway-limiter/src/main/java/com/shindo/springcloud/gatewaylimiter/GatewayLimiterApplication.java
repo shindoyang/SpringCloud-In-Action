b1e5ef384273f9fec3dbc03c59db34aadcb77566
unknown <yanggeng@ut.cn>
2020-07-08 14:03:23 +0800

    新增gateway-limiter 子modules

---
+++ gateway-limiter/src/main/java/com/shindo/springcloud/gatewaylimiter/GatewayLimiterApplication.java
@@ -0,0 +1,13 @@
+package com.shindo.springcloud.gatewaylimiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayLimiterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayLimiterApplication.class, args);
+    }
+
+}
