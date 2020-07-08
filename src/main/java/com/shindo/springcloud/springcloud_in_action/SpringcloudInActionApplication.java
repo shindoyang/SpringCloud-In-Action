b5f22f697dc31fc9d250cfe579670b1d7b595062
unknown <yanggeng@ut.cn>
2020-07-08 09:47:36 +0800

    新建根项目

---
+++ src/main/java/com/shindo/springcloud/springcloud_in_action/SpringcloudInActionApplication.java
@@ -0,0 +1,13 @@
+package com.shindo.springcloud.springcloud_in_action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudInActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudInActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shindo/springcloud/springcloud_in_action/SpringcloudInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shindo.springcloud.springcloud_in_action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudInActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
