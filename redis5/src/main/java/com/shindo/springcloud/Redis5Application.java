f5382e2f2c92b1bfd8bcd5cf3a00c2309fbccbc6
shindoyang <727792353@qq.com>
2020-07-13 23:36:05 +0800

    新增mq和redismodules

---
+++ redis5/src/main/java/com/shindo/springcloud/Redis5Application.java
@@ -0,0 +1,13 @@
+package com.shindo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Redis5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Redis5Application.class, args);
+	}
+
+}
